072518be145746d3986785c939cd2e777ad73be3
labib <labibhs01@gamil.com>
2023-10-17 14:50:34 +0700

    Search + pagination

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,28 +2,35 @@
 
 namespace App\Http\Livewire;
 use App\Models\ViewBooks;
-
+use Livewire\WithPagination;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
-    // use WithPagination;
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
 
     public $search = '';
 
+
     public function render()
     {
-        $viewBooks = ViewBooks::all()
-        ->where(function($query){
+        $query = $this->search;
+        $viewBooks = ViewBooks::where(function($query){
             $query->where('isbn','like','%'.$this->search.'%')
             ->orwhere('author','like','%'.$this->search.'%')
             ->orwhere('title','like','%'.$this->search.'%')
             ->orwhere('price','like','%'.$this->search.'%');
-        });
-        // ->orderBy('id','ASC');
-        // ->paginate(10);
+        })
+        ->orderBy('id','ASC')
+        ->paginate(10);
+        // ->get();
 
-        return view('livewire.dashboard', ['view_books' => $viewBooks]);
+        return view('livewire.dashboard', [
+            'view_books' => $viewBooks,
+            'search' => $this->search
+        ]);
     }
 
 
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,78 +1,108 @@
- <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
     <div class="container-fluid py-4">
-      <div class="row">
-                <h5 class="font-weight-bolder" style="font-size: 24px;">Selamat Datang di Bookorama &#x1F4DA;</h5>
-                <br>
-                <br>
-                    <div class="col-12">
-                        <div class="card mb-4 mx-4">
-                            <div class="card-header pb-0">
-                                <div class="d-flex flex-row justify-content-between">
-                                    <div>
-                                        <h5 class="mb-0" style="text-align: center;">Rekomendasi Buku Untukmu !</h5>
-                                    </div>
-                                    <div class="search-bar">
-                                        <input  type="text" wire:model='search' placeholder="Search..." id="search-input">
-                                        <button class= "btn btn-primary" id="search-button">Search</button>
-                                    </div>
-                                </div>
+        <div class="row">
+            <h5 class="font-weight-bolder" style="font-size: 24px;">Selamat Datang di Bookorama &#x1F4DA;</h5>
+            <br>
+            <br>
+            <div class="col-12">
+                <div class="card mb-4 mx-4">
+                    <div class="card-header pb-0">
+                        <div class="d-flex flex-row justify-content-between">
+                            <div>
+                                <h5 class="mb-0" style="text-align: center;">Rekomendasi Buku Untukmu !</h5>
+                                {{-- <h5>Search Query: '{{ $search }}'</h5> --}}
                             </div>
-                            <br>
-                            <div class="card-body px-0 pt-0 pb-2">
-                                <div class="table-responsive p-0">
-                                    <table class="table align-items-center mb-0">
-                                        <thead>
-                                            <tr>
-                                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                                ID
-                                                </th>
-                                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                                ISBN
-                                                </th>
-                                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                                Author
-                                                </th>
-                                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                                Title
-                                                </th>
-                                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                                Price (Rupiah)
-                                                </th>
-                                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                                Action
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <?php
+                            <div class="search-bar">
+                                <div class="input-group">
+                                    <span class="input-group-text text-body"><i class="fas fa-search"
+                                            aria-hidden="true"></i></span>
+                                    <input type="search" wire:model.live='search' class="form-control"
+                                        placeholder="Search...">
+                                </div>
+                                {{-- <input type="search" wire:model='search' placeholder="Search..." id="search-input">
+                                <button class="btn btn-primary" id="search-button">Search</button> --}}
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                    <br>
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <div class="table-responsive p-0">
+                            <table class="table align-items-center mb-0">
+                                <thead>
+                                    <tr>
+                                        {{-- <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            No
+                                        </th> --}}
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            ID
+                                        </th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            ISBN
+                                        </th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Author
+                                        </th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Title
+                                        </th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Price (Rupiah)
+                                        </th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <?php
                                         // if (DB::connection()->getPdo()) {
                                         //     echo "Berhasil terhubung ke database: " . DB::connection()->getDatabaseName();
                                         // }
                                         ?>
-                                        @foreach($view_books as $book)
-                                        <tr>
-                                            <td style="text-align: center;">{{ $book->id }}</td>
-                                            <td style="text-align: center;">{{ $book->isbn }}</td>
-                                            <td style="text-align: center;">{{ $book->author }}</td>
-                                            <td style="text-align: center;">{{ $book->title }}</td>
-                                            <td style="text-align: center;">{{ $book->price }}</td>
-                                            <td style="text-align: center;">
-                                                <a class="badge badge-sm bg-gradient-success">Add to Cart</a>
-                                                <a class="badge badge-sm bg-gradient-secondary">Details</a>
-                                            </td>
-                                        </tr>
-                                         @endforeach
+                                @foreach($view_books as $book)
+                                <tr>
+                                    {{-- <td class="ps-4" style="text-align: center;">
+                                        <p class="text-xs font-weight-bold mb-0">{{ ($view_books ->currentpage()-1) *
+                                            $view_books ->perpage() + $loop->index + 1 }}</p>
+                                    </td> --}}
+                                    {{-- <td style="text-align: center;">{{ ($view_books ->currentpage()-1) *
+                                        $view_books ->perpage() + $loop->index + 1 }}</td> --}}
+                                    <td style="text-align: center;">{{ $book->id }}</td>
+                                    <td style="text-align: center;">{{ $book->isbn }}</td>
+                                    <td style="text-align: center;">{{ $book->author }}</td>
+                                    <td style="text-align: center;">{{ $book->title }}</td>
+                                    <td style="text-align: center;">{{ $book->price }}</td>
+                                    <td style="text-align: center;">
+                                        <a class="badge badge-sm bg-gradient-success">Add to Cart</a>
+                                        <a class="badge badge-sm bg-gradient-secondary">Details</a>
+                                    </td>
+                                </tr>
+                                @endforeach
 
-                                    </table>
-                                </div>
+                            </table>
+                            <div class="mt-3 mx-3 d-flex align-items-center justify-content-center text-danger">
+                                {{ $view_books->links() }}
+
                             </div>
-        </div>
-    </div>
-  </main>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</main>
 
-  <!--   Core JS Files   -->
-  <script src="/assets/js/plugins/chartjs.min.js"></script>
-  <script src="/assets/js/plugins/Chart.extension.js"></script>
-  <script>
+<!--   Core JS Files   -->
+<script src="/assets/js/plugins/chartjs.min.js"></script>
+<script src="/assets/js/plugins/Chart.extension.js"></script>
+<script>
     var ctx = document.getElementById("chart-bars").getContext("2d");
 
     new Chart(ctx, {
@@ -226,4 +256,4 @@
         },
       },
     });
-  </script>
+</script>
