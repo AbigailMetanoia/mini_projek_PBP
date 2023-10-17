28c8012a96b2632bd0b8f14dddb57e197d1268a8
labib <labibhs01@gamil.com>
2023-10-17 12:51:19 +0700

    Search

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -7,9 +7,18 @@ use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $search = '';
+
     public function render()
     {
-        $viewBooks = ViewBooks::all();
+        $viewBooks = ViewBooks::where(function($query){
+            $query->where('isbn','like','%'.$this->search.'%')
+            ->orwhere('author','like','%'.$this->search.'%')
+            ->orwhere('title','like','%'.$this->search.'%')
+            ->orwhere('price','like','%'.$this->search.'%');
+        })
+        ->orderBy('id','ASC');
+
 
         return view('livewire.dashboard', ['view_books' => $viewBooks]);
     }
